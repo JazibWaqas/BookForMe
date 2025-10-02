550ed47a88db6645b0b1a95456be117bdc07d528
JazibWaqas <jazib.waqas@gmail.com>
2025-10-02 12:42:27 +0500

    routes fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage'
+import Chat from './pages/Chat'
+import Social from './pages/Social'
+import Notifications from './pages/Notifications'
 import VendorPage from './pages/VendorPage'
 import './App.css'
 
@@ -10,6 +12,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Homepage />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/social" element={<Social />} />
+          <Route path="/notifications" element={<Notifications />} />
           <Route path="/vendor/:id" element={<VendorPage />} />
         </Routes>
       </Router>
